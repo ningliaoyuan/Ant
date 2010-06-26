40726a009e8a40726b6eb1b95255ac167a71901c
lanslot <lanslot.liu@gmail.com>
2010-06-26 21:42:14 +0800

    rewrite css and add logon popup
    Signed-off-by: lanslot <lanslot.liu@gmail.com>

--- AntSrc/Ant/Scripts/antaeus.kaogmat.js
+++ AntSrc/Ant/Scripts/antaeus.kaogmat.js
@@ -1,6 +1,13 @@
 jQuery(document).ready(function($) {
 
-	$("*[ajaxstatus='loading']").hide();
+$("*[ajaxstatus='loading']").hide();
+	
 
+var Logon = $("#DivLogon").overlay({
+	top: 150,
+	mask: {color:"#000",loadSpeed:200,opacity:0.5},
+	closeOnClick: false
+});
+$("#LinkLogon").click(function(e){Logon.overlay().load();});
 
 });
