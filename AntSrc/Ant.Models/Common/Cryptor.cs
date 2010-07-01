91e0f5dd6beb45cacdce47189ec9a55c7e3cc399
ningliaoyuan <ninglioayuan@gmail.com>
2010-07-02 02:44:58 +0800

    Add Cryptor class for future use
    Signed-off-by: ningliaoyuan <ninglioayuan@gmail.com>

---
+++ AntSrc/Ant.Models/Common/Cryptor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Ant.Models.Common
+{
+    public static class Encryptor
+    {
+        private static byte[] KEY = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6,
+            7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2
+        };
+        private static byte[] IV = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6 };
+
+        public static string Encrypt(string encryptString)
+        {
+            try
+            {
+                byte[] inputByteArray = Encoding.UTF8.GetBytes(encryptString);
+
+                Rijndael cryptor = Rijndael.Create();
+                MemoryStream mStream = new MemoryStream();
+                CryptoStream cStream = new CryptoStream(mStream, cryptor.CreateEncryptor(KEY, IV), CryptoStreamMode.Write);
+                cStream.Write(inputByteArray, 0, inputByteArray.Length);
+                cStream.FlushFinalBlock();
+                return Convert.ToBase64String(mStream.ToArray());
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public static string Decrypt(string decryptString)
+        {
+            try
+            {
+                byte[] inputByteArray = Convert.FromBase64String(decryptString);
+
+                Rijndael cryptor = Rijndael.Create();
+                MemoryStream mStream = new MemoryStream();
+                CryptoStream cStream = new CryptoStream(mStream, cryptor.CreateDecryptor(KEY, IV), CryptoStreamMode.Write);
+                cStream.Write(inputByteArray, 0, inputByteArray.Length);
+                cStream.FlushFinalBlock();
+                return Encoding.UTF8.GetString(mStream.ToArray());
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ AntSrc/Ant.Tests/EncryptorTest.cs
@@ -0,0 +1,91 @@
+﻿using Ant.Models.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Ant.Tests
+{
+    /// <summary>
+    ///This is a test class for EncryptorTest and is intended
+    ///to contain all EncryptorTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class EncryptorTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for Encrypt
+        ///</summary>
+        [TestMethod()]
+        public void EncryptTest()
+        {
+            string encryptString = "ningliaoyuan";
+            string actual;
+            actual = Encryptor.Encrypt(encryptString);
+            Assert.AreNotEqual(encryptString, actual);
+        }
+
+        /// <summary>
+        ///A test for Decrypt
+        ///</summary>
+        [TestMethod()]
+        public void DecryptTest()
+        {
+            string encryptString = "ningliaoyuan";
+
+            string decryptString = Encryptor.Encrypt(encryptString);
+            string actual;
+            actual = Encryptor.Decrypt(decryptString);
+            Assert.AreEqual(encryptString, actual);
+        }
+    }
+}
